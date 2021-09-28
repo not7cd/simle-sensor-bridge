d44b6521fc1e44ea85d7897d4020182d5c67dfdd
not7cd <norbert@not7cd.net>
2021-09-28 08:51:18 +0200

    Move to XIAO MCP2515

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,6 @@
 
 //------------------------------- Board Check ----------------------------------
 
-#ifndef ARDUINO_ARCH_ESP32
-#error "Select an ESP32 board"
-#endif
 
 /**************************************************************************************
  * INCLUDES
@@ -13,11 +10,8 @@
 #include <SPI.h>
 #include <Wire.h>              //for ESP8266 use bug free i2c driver https://github.com/enjoyneering/ESP8266-I2C-Driver
 
-#include <core_version.h> // For ARDUINO_ESP32_RELEASE
-
 #include <ArduinoUAVCAN.h>
 #include <ArduinoMCP2515.h>
-#include <ACAN_ESP32.h>
 
 #include <OneWire.h>
 #include <DallasTemperature.h>
@@ -56,15 +50,18 @@ static const uavcan_node_GetInfo_Response_1_0 GET_INFO_DATA = {
 };
 
 static const uint32_t DESIRED_BIT_RATE = 125UL * 1000UL; // 1 kb/s
-static const int LED_BUILTIN = 2;
+// static const int LED_BUILTIN = 2;
 
-static const gpio_num_t CTX_PIN = GPIO_NUM_4;
-static const gpio_num_t CRX_PIN = GPIO_NUM_5;
+static int const MKRCAN_MCP2515_CS_PIN  = A1;
+static int const MKRCAN_MCP2515_INT_PIN = A2;
 
-static const gpio_num_t TEMP_ONE_WIRE_PIN = GPIO_NUM_15;
+static int const TEMP_ONE_WIRE_PIN = A3;
 
-static CanardPortID const TEMP_PORT_ID   = 2137U;
+static CanardPortID const TEMP_A_PORT_ID   = 2137U;
+static CanardPortID const TEMP_B_PORT_ID   = 2138U;
 static CanardPortID const LOADCELL_PORT_ID   = 1337U;
+
+static int const TEMPERATURE_PRECISION = 12;
 
 
 
@@ -72,34 +69,56 @@ static CanardPortID const LOADCELL_PORT_ID   = 1337U;
  * FUNCTION DECLARATION
  **************************************************************************************/
 
+void    spi_select        ();
+void    spi_deselect       ();
+uint8_t spi_transfer       (uint8_t const);
+void    onExternalEvent    ();
 bool transmitCanFrame(CanardFrame const &);
-void onReceiveCanFrame(CANMessage const &);
+void    onReceiveBufferFull(CanardFrame const &);
+
 void onGetInfo_1_0_Request_Received(CanardTransfer const &, ArduinoUAVCAN &);
 void onLoadcell_1_0_Received(CanardTransfer const &, ArduinoUAVCAN &);
 
 void on_i2c_request();
 
-void get_temp(DeviceAddress dev);
+void get_temp();
 
-void print_ESP_chip_info();
-void print_ESP_CAN_info(ACAN_ESP32_Settings &settings);
+// function to print a device address
+void printAddress(DeviceAddress deviceAddress)
+{
+  for (uint8_t i = 0; i < 8; i++)
+  {
+    // zero pad the address if necessary
+    if (deviceAddress[i] < 16) Serial.print("0");
+    Serial.print(deviceAddress[i], HEX);
+  }
+}
 
 /**************************************************************************************
  * GLOBAL VARIABLES
  **************************************************************************************/
 
+ArduinoMCP2515 mcp2515(spi_select,
+                       spi_deselect,
+                       spi_transfer,
+                       micros,
+                       onReceiveBufferFull,
+                       nullptr);
+
 ArduinoUAVCAN uc(UC_ID, transmitCanFrame);
 
 Heartbeat_1_0<> hb;
 Real32_1_0<LOADCELL_PORT_ID> scale_measurment;
-Real32_1_0<TEMP_PORT_ID> temperature_measurment;
+//TODO: refactor to list
+Real32_1_0<TEMP_A_PORT_ID> tempAbove;
+Real32_1_0<TEMP_B_PORT_ID> tempBelow;
 
 static uint32_t gReceivedFrameCount = 0;
 static uint32_t gSentFrameCount = 0;
 
 OneWire oneWire(TEMP_ONE_WIRE_PIN);
 DallasTemperature sensors(&oneWire);
-DeviceAddress insideThermometer;
+DeviceAddress thermoAbove, thermoBelow;
 
 /**************************************************************************************
  * SETUP/LOOP
@@ -108,7 +127,8 @@ DeviceAddress insideThermometer;
 void setup()
 {
   int error;
-
+  
+ 
   Wire.begin(8);
   Wire.onRequest(on_i2c_request);
   //--- Switch on builtin led
@@ -116,47 +136,49 @@ void setup()
   digitalWrite(LED_BUILTIN, HIGH);
   //--- Start serial
   Serial.begin(115200);
-  while (!Serial)
-  {
-  }
+  // while (!Serial)
+  // {
+  // }
+  /* Setup SPI access */
+  SPI.begin();
+  pinMode(MKRCAN_MCP2515_CS_PIN, OUTPUT);
+  digitalWrite(MKRCAN_MCP2515_CS_PIN, HIGH);
 
+  /* Attach interrupt handler to register MCP2515 signaled by taking INT low */
+  pinMode(MKRCAN_MCP2515_INT_PIN, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(MKRCAN_MCP2515_INT_PIN), onExternalEvent, FALLING);
 
-  print_ESP_chip_info();
-
-  //--- Configure ESP32 CAN
-  Serial.println("Configure ESP32 CAN");
-  ACAN_ESP32_Settings settings(DESIRED_BIT_RATE);
-  settings.mRxPin = CRX_PIN;
-  settings.mTxPin = CTX_PIN;
-  const uint32_t errorCode = ACAN_ESP32::can.begin(settings);
-
-  if (errorCode == 0)
-  {
-    print_ESP_CAN_info(settings);
-  }
-  else
-  {
-    Serial.print("Configuration error 0x");
-    Serial.println(errorCode, HEX);
-  }
+  /* Initialize MCP2515 */
+  mcp2515.begin();
+  mcp2515.setBitRate(CanBitRate::BR_125kBPS_8MHZ);
+  mcp2515.setNormalMode();
 
     // locate devices on the bus
   Serial.print("Locating devices...");
+   delay(4000);
   sensors.begin();
   Serial.print("Found ");
   Serial.print(sensors.getDeviceCount(), DEC);
   Serial.println(" devices.");
-  if (!sensors.getAddress(insideThermometer, 0)) Serial.println("Unable to find address for Device 0"); 
-  
+
+    oneWire.reset_search();
+  if (!oneWire.search(thermoAbove)) Serial.println("Unable to find address for Device 0"); 
+  if (!oneWire.search(thermoBelow)) Serial.println("Unable to find address for Device 1");
+
   Serial.print("Device 0 Address: ");
-  printAddress(insideThermometer);
+  printAddress(thermoAbove);
+  Serial.println();
+
+  Serial.print("Device 1 Address: ");
+  printAddress(thermoBelow);
   Serial.println();
 
   // set the resolution to 9 bit (Each Dallas/Maxim device is capable of several different resolutions)
-  sensors.setResolution(insideThermometer, 9);
+  sensors.setResolution(thermoAbove, TEMPERATURE_PRECISION);
+  sensors.setResolution(thermoBelow, TEMPERATURE_PRECISION);
  
   Serial.print("Device 0 Resolution: ");
-  Serial.print(sensors.getResolution(insideThermometer), DEC); 
+  Serial.print(sensors.getResolution(thermoAbove), DEC); 
   Serial.println();
 
   /* Configure initial heartbeat */
@@ -174,16 +196,15 @@ void loop()
   /* Update the heartbeat object */
   hb.data.uptime = millis() / 1000;
   hb = Heartbeat_1_0<>::Mode::OPERATIONAL;
-  CANMessage frame;
 
   /* Publish the heartbeat once/second */
   static unsigned long prev = 0;
   unsigned long const now = millis();
   if (now - prev >= 1000)
   {
-    get_temp(insideThermometer);
+    get_temp();
     uc.publish(hb);
-    uc.publish(temperature_measurment);
+    uc.publish(tempAbove);
     prev = now;
 
     digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
@@ -196,22 +217,11 @@ void loop()
     Serial.print("Receive: ");
     Serial.print(gReceivedFrameCount);
     Serial.print("\t");
-    Serial.print(" STATUS 0x");
-    Serial.print(CAN_STATUS, HEX);
-    Serial.print(" RXERR ");
-    Serial.print(CAN_RX_ECR);
-    Serial.print(" TXERR ");
-    Serial.println(CAN_TX_ECR);
+
   }
     /* Transmit all enqeued CAN frames */
   while (uc.transmitCanFrame())
   {
-  }
-
-  while (ACAN_ESP32::can.receive(frame))
-  {
-    onReceiveCanFrame(frame);
-    gReceivedFrameCount += 1;
   }
 }
 
@@ -220,16 +230,16 @@ void loop()
  **************************************************************************************/
 
 void on_i2c_request() {
-  Serial.println("i2c");
+  Serial.print(".");
   float vec[2] = {
-    (float) temperature_measurment.data.value, 
-    (float) temperature_measurment.data.value // TODO: second measure
+    (float) tempAbove.data.value, 
+    (float) tempBelow.data.value
   };
   Wire.write((uint8_t*) vec, sizeof(vec));
 }
 
 // function to print the temperature for a device
-void get_temp(DeviceAddress deviceAddress)
+void get_temp()
 {
   sensors.requestTemperatures(); // Send the command to get temperatures
   // method 1 - slower
@@ -239,55 +249,58 @@ void get_temp(DeviceAddress deviceAddress)
   //Serial.print(sensors.getTempF(deviceAddress)); // Makes a second call to getTempC and then converts to Fahrenheit
 
   // method 2 - faster
-  float tempC = sensors.getTempC(deviceAddress);
-  if(tempC == DEVICE_DISCONNECTED_C) 
+  float tempA = sensors.getTempC(thermoAbove);
+  if(tempA == DEVICE_DISCONNECTED_C) 
   {
     Serial.println("Error: Could not read temperature data");
     return;
   }
-  temperature_measurment.data.value = tempC;
-  Serial.print("Temp C: ");
-  Serial.println(tempC);
+  tempAbove.data.value = tempA;
+  float tempB = sensors.getTempC(thermoBelow);
+  if(tempB == DEVICE_DISCONNECTED_C) 
+  {
+    Serial.println("Error: Could not read temperature data");
+    return;
+  }
+  tempBelow.data.value = tempB;
+  printAddress(thermoAbove);
+  Serial.print(" ");
+  Serial.println(tempA);
+  printAddress(thermoBelow);
+  Serial.print(" ");
+  Serial.println(tempB);
 }
 
-bool transmitCanFrame(CanardFrame const &frame)
+void spi_select()
 {
-  CANMessage frame2;
-  frame2.id = frame.extended_can_id;
-  frame2.ext = true;
-
-  // TODO: this can be fucking better
-  const void *p = frame.payload;
-  for (size_t i = 0; i < frame.payload_size; i++)
-  {
-    frame2.data[i] = *(uint8_t *) p;
-    p++;
-  }
-  
-  frame2.len = static_cast<uint8_t const>(frame.payload_size);
-
-  // Serial.print("TX ");
-  // Serial.println(frame2.id);
-  const bool ok = ACAN_ESP32::can.tryToSend(frame2);
-  if (ok)
-  {
-    gSentFrameCount += 1;
-  }
-  return ok;
+  digitalWrite(MKRCAN_MCP2515_CS_PIN, LOW);
 }
 
-void onReceiveCanFrame(CANMessage const &frame)
+void spi_deselect()
 {
-  CanardFrame const f
-    {
-      1,                        /* timestamp_usec  */
-      frame.id & MCP2515::CAN_ADR_BITMASK,       /* extended_can_id limited to 29 bit */
-      static_cast<uint8_t const>(frame.len),     /* payload_size    */
-      reinterpret_cast<const void *>(frame.data) /* payload         */
-    };
-  // Serial.print("RX ");
-  // Serial.println(frame.id);
-  uc.onCanFrameReceived(f);
+  digitalWrite(MKRCAN_MCP2515_CS_PIN, HIGH);
+}
+
+uint8_t spi_transfer(uint8_t const data)
+{
+  return SPI.transfer(data);
+}
+
+void onExternalEvent()
+{
+  mcp2515.onExternalEventHandler();
+}
+
+bool transmitCanFrame(CanardFrame const & frame)
+{
+  gSentFrameCount += 1;
+  return mcp2515.transmit(frame);
+}
+
+void onReceiveBufferFull(CanardFrame const & frame)
+{
+  gReceivedFrameCount += 1;
+  uc.onCanFrameReceived(frame);
 }
 
 void onLoadcell_1_0_Received(CanardTransfer const & transfer, ArduinoUAVCAN & uc) {
@@ -304,48 +317,3 @@ void onGetInfo_1_0_Request_Received(CanardTransfer const & transfer, ArduinoUAVC
   uc.respond(rsp, transfer.remote_node_id, transfer.transfer_id);
 }
 
-
-void print_ESP_chip_info()
-{
-  esp_chip_info_t chip_info;
-  esp_chip_info(&chip_info);
-  Serial.print("ESP32 Arduino Release: ");
-  Serial.println(ARDUINO_ESP32_RELEASE);
-  Serial.print("ESP32 Chip Revision: ");
-  Serial.println(chip_info.revision);
-  Serial.print("ESP32 SDK: ");
-  Serial.println(ESP.getSdkVersion());
-  Serial.print("ESP32 Flash: ");
-  Serial.print(spi_flash_get_chip_size() / (1024 * 1024));
-  Serial.print(" MB ");
-  Serial.println(((chip_info.features & CHIP_FEATURE_EMB_FLASH) != 0) ? "(embeded)" : "(external)");
-  Serial.print("APB CLOCK: ");
-  Serial.print(APB_CLK_FREQ);
-  Serial.println(" Hz");
-}
-
-void print_ESP_CAN_info(ACAN_ESP32_Settings &settings)
-{
-  Serial.print("Bit Rate prescaler: ");
-  Serial.println(settings.mBitRatePrescaler);
-  Serial.print("Time Segment 1:     ");
-  Serial.println(settings.mTimeSegment1);
-  Serial.print("Time Segment 2:     ");
-  Serial.println(settings.mTimeSegment2);
-  Serial.print("RJW:                ");
-  Serial.println(settings.mRJW);
-  Serial.print("Triple Sampling:    ");
-  Serial.println(settings.mTripleSampling ? "yes" : "no");
-  Serial.print("Actual bit rate:    ");
-  Serial.print(settings.actualBitRate());
-  Serial.println(" bit/s");
-  Serial.print("Exact bit rate ?    ");
-  Serial.println(settings.exactBitRate() ? "yes" : "no");
-  Serial.print("Distance            ");
-  Serial.print(settings.ppmFromDesiredBitRate());
-  Serial.println(" ppm");
-  Serial.print("Sample point:       ");
-  Serial.print(settings.samplePointFromBitStart());
-  Serial.println("%");
-  Serial.println("Configuration OK!");
-}
